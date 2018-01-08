bfbfae79ad6c35563e1dfe5309c4166c7c0c570b
ROHIT <rohitraju3010>
2018-01-08 17:34:08 +0530

    with design life cardView

--- app/src/main/java/com/example/anuja/reall/DesignALife.java
+++ app/src/main/java/com/example/anuja/reall/DesignALife.java
@@ -1,12 +1,68 @@
 package com.example.anuja.reall;
 
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.anuja.reall.Adapter.DesignLifeAdapter;
+import com.example.anuja.reall.Adapter.FirstNameAdapter;
+import com.example.anuja.reall.Model.DesignLifeModel;
+import com.example.anuja.reall.Model.FirstName;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DesignALife extends AppCompatActivity {
+
+    ArrayList<RadioButton> rba=new ArrayList<RadioButton>();
+    static int length;
+    ColorStateList colorStateList = null;
+    public static JSONObject countryobj[]=new JSONObject[350];
+    public static JSONObject retobj;
+    public static String countryname;
+    HashMap<String,Integer> details=new HashMap<>();
+
+    String url=Constant.ACTIONURL+"/residence/country";
+
+    public static DesignLifeModel selected_country=new DesignLifeModel();
+    private DesignLifeAdapter dladapter;
+    private RecyclerView recyclerView;
+
+     public List<DesignLifeModel> myarray=new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +71,146 @@ public class DesignALife extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_designlife);
 
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(DesignALife.this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        getCountries();
+
+
+
+
+    }
+
+    public void setCount(String s)
+    {
+       countryname=s;
+       System.out.println("countindesign"+countryname);
     }
     public void tomainactivity(View view) {
         Intent intent = new Intent(DesignALife.this, MainActivity.class);
         startActivity(intent);
     }
 
+
+
     public void goto_selectcity(View view) {
+
+        for(int i=0;i<length;i++)
+        {
+            try {
+                if(countryobj[i].getString("countryName").equals(countryname))
+                {
+                    retobj=countryobj[i];
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        //Log.e("JSON Object",retobj);
         Intent intent = new Intent(DesignALife.this, Selectcity.class);
+        intent.putExtra("countryObject",retobj.toString());
         startActivity(intent);
+
+
     }
+
+
+
+    void getCountries()
+    {
+
+
+        RequestQueue que= Volley.newRequestQueue(DesignALife.this);
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(final JSONArray response) {
+
+                        length=response.length();
+                        JSONObject obj = null;
+
+
+
+                        for(int i=0;i<response.length();i++)
+                        {
+                            try {
+                                //myarray.add(((JSONObject) response.get(i)).getString("countryName"));
+
+                                obj=(JSONObject) response.get(i);
+                                countryobj[i]=obj;
+                                Log.e("DATA",obj.getString("countryName"));
+
+
+                                DesignLifeModel countryName=new DesignLifeModel(obj.getString("countryName"));
+                                myarray.add(countryName);
+                                ;
+                                //details.put(obj.getString("countryName"),obj.getInt("countryid"));
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+
+                        DesignLifeAdapter dladapter=new DesignLifeAdapter(myarray);
+                        recyclerView.setAdapter(dladapter);
+
+
+                    }
+
+
+
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("Options", MODE_PRIVATE); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+                String token=pref.getString("token",null);
+                Log.e("x-auth-token",token);
+
+                HashMap<String,String> headers=new HashMap<>();
+                headers.put("Content-Type","application/json");
+                headers.put("x-auth-token",token);
+                return headers;
+            }
+
+
+        };
+
+        req.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return  20000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 20000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+
+        que.add(req);
+
+    }
+
+
+
 }
+
--- app/src/main/java/com/example/anuja/reall/Selectcity.java
+++ app/src/main/java/com/example/anuja/reall/Selectcity.java
@@ -1,12 +1,64 @@
 package com.example.anuja.reall;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.anuja.reall.Adapter.DesignLifeAdapter;
+import com.example.anuja.reall.Adapter.SelectCityAdapter;
+import com.example.anuja.reall.Model.DesignLifeModel;
+import com.example.anuja.reall.Model.SelectCityModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Selectcity extends AppCompatActivity {
+
+    ArrayList<RadioButton> rba=new ArrayList<RadioButton>();
+    static int length;
+    static JSONObject countryObject;
+    public static JSONObject retobj;
+    public static String cityname;
+    public static JSONObject cityObject[]=new JSONObject[350];
+    ColorStateList colorStateList = null;
+
+
+     List<SelectCityModel> myarray=new ArrayList<>();
+
+    public static SelectCityModel selected_city=new SelectCityModel();
+    private SelectCityAdapter scadapter;
+    private RecyclerView recyclerView;
+
+    String url=Constant.GAMEURL+"characterDesign/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +66,25 @@ public class Selectcity extends AppCompatActivity {
         setContentView(R.layout.activity_selectcity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent i=getIntent();
+        try {
+            countryObject=new JSONObject(i.getStringExtra("countryObject"));
+            //Log.e("CountryObject",countryObject.toString());
+            url+=String.valueOf(countryObject.getString("countryid"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_selectcity);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(Selectcity.this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        getCities();
+
 
     }
     public void tomainactivity(View view) {
@@ -25,7 +96,104 @@ public class Selectcity extends AppCompatActivity {
         startActivity(intent);
     }
     public void toselectattr(View view) {
+        for(int i=0;i<length;i++)
+        {
+            try {
+                if(cityObject[i].getString("cityName").equals(cityname))
+                {
+                    retobj=cityObject[i];
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
         Intent intent = new Intent(Selectcity.this,AttributesActivity.class);
+        intent.putExtra("countryObject",countryObject.toString());
+        intent.putExtra("cityObject",retobj.toString());
         startActivity(intent);
     }
+
+
+    public void getCityName(String s)
+    {
+        cityname=s;
+        System.out.println("citynameinselectcity"+cityname);
+
+    }
+
+
+    void getCities()
+    {
+
+        RequestQueue que= Volley.newRequestQueue(Selectcity.this);
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        length=response.length();
+                        JSONObject obj = null;
+
+
+                        for(int i=0;i<response.length();i++)
+                        {
+                            try {
+                                //myarray.add(((JSONObject) response.get(i)).getString("countryName"));
+
+                                obj=(JSONObject) response.get(i);
+                                cityObject[i]=obj;
+                                Log.e("DATA",obj.getString("cityName"));
+
+                                SelectCityModel cityName=new SelectCityModel(obj.getString("cityName"));
+                                myarray.add(cityName);
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+
+                        SelectCityAdapter scadapter=new SelectCityAdapter(myarray);
+                        recyclerView.setAdapter(scadapter);
+
+                    }
+
+
+
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("Options", MODE_PRIVATE); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+                String token=pref.getString("token",null);
+                Log.e("x-auth-token",token);
+
+                HashMap<String,String> headers=new HashMap<>();
+                headers.put("Content-Type","application/json");
+                headers.put("x-auth-token",token);
+                return headers;
+            }
+
+
+        };
+
+        que.add(req);
+
+    }
+
+
+
 }
+
+
